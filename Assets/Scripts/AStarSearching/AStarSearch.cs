8bb7caadce71bb9be141fe95d8de0ee1dc96a270
Charlie (Laptop) <P2423910@my365.dmu.ac.uk>
2021-05-03 18:13:07 +0100

    Attempt to revert braking.

--- Assets/Scripts/AStarSearching/AStarSearch.cs
+++ Assets/Scripts/AStarSearching/AStarSearch.cs
@@ -72,11 +72,14 @@ public class AStarSearch : MonoBehaviour
         }
     }
 
+<<<<<<< HEAD
     //public bool IsPositionTraversable(Vector3 worldPos)
     //{
     //    return !(Physics.CheckBox(worldPos, new Vector3(nodeSize, nodeSize, nodeSize) * 0.5f, Quaternion.identity, obstacleLayerMask));
     //}
 
+=======
+>>>>>>> parent of 8dea2d3 (Reduced dog collisions.)
     /** \fn RequestPath
     *  \brief Returns an A* found path between 2 input GameObjects.
     *  \param startObject The object to find a path FROM.
