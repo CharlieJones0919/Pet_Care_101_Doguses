01128055fe53e76f9aad4a9c2328ed57bfd9c0e1
Charlie (Laptop) <P2423910@my365.dmu.ac.uk>
2021-05-03 18:17:20 +0100

    Revert "Added non-traversable space to edge of grid."
    This reverts commit c3a1e0a87f9cf6c7052e0fedd6ecf94be7735f1d.

--- Assets/Scripts/UI/StoreController.cs
+++ Assets/Scripts/UI/StoreController.cs
@@ -11,9 +11,8 @@ public class StoreController : MonoBehaviour
 {
     [SerializeField] private DogController controller;
     [SerializeField] private AStarSearch worldFloor;
-    [SerializeField] private List<Vector3> permanantItemPositions = new List<Vector3>();
-    [SerializeField] private float traversableFloorOffset;
-
+    [SerializeField] private List<Vector3> perminantItemPositions = new List<Vector3>();
+    
     [SerializeField] private string itemPrefabBaseDir = "Prefabs/Items";
     [SerializeField] private UnityEngine.Object[] itemPrefabs;
 
@@ -92,10 +91,10 @@ public class StoreController : MonoBehaviour
             }
         }
 
-        float floorStartX = (-worldFloor.transform.localScale.x / 2.0f) + traversableFloorOffset;
-        float floorStartZ = (worldFloor.transform.localScale.z / 2.0f) - traversableFloorOffset;
-        float floorEndX = (worldFloor.transform.localScale.x / 2.0f) - traversableFloorOffset;
-        float floorEndZ = worldFloor.transform.position.z + traversableFloorOffset;
+        float floorStartX = (-worldFloor.transform.localScale.x / 2.0f) + 0.5f;
+        float floorStartZ = (worldFloor.transform.localScale.z / 2.0f) - 0.5f;
+        float floorEndX = (worldFloor.transform.localScale.x / 2.0f) - 0.5f;
+        float floorEndZ = worldFloor.transform.position.z + 0.5f;
         int numPossiblePositions = 1;
 
         float posX = floorStartX;
@@ -107,7 +106,7 @@ public class StoreController : MonoBehaviour
 
             if ((posX <= floorEndX))
             {
-                permanantItemPositions.Add(new Vector3(posX, 0, posZ));
+                perminantItemPositions.Add(new Vector3(posX, 0, posZ));
                 posX += 1.0f;
             }
             else if (posZ > floorEndZ)
@@ -126,10 +125,10 @@ public class StoreController : MonoBehaviour
     {
         if (!focusItem.IsSingleUse())
         {
-            if (permanantItemPositions.Count > 0)
+            if (perminantItemPositions.Count > 0)
             {
-                focusItem.ActivateAvailableInstanceTo(permanantItemPositions[0]);
-                permanantItemPositions.Remove(permanantItemPositions[0]);
+                focusItem.ActivateAvailableInstanceTo(perminantItemPositions[0]);
+                perminantItemPositions.Remove(perminantItemPositions[0]);
             }
             else { controller.tipPopUp.DisplayTipMessage("The shelter has no more room to place anymore items."); }
         }
